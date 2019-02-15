641f5769ef489d9e92e4d5ec276fccea9dc3c404
Ursula <ursularuizvera@gmail.com>
2019-02-15 11:28:34 -0600

    new updated

--- conversion.py
+++ conversion.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 
 cabnmsdmm,ndms
 dsafjhjsdhflskhflHFASDHFADl   dkdsjfhkjGFYUIEW
-=======
- cabnm changes to middle of line sdmm,ndms
->>>>>>> a2380439ae2c99626066a5721934e5d9340ca194
+
+
